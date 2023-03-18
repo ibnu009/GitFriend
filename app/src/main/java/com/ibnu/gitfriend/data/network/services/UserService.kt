0f16c09fcadae6ea6982a8e5910a20bdbe08b991
ibnu001 <ibnubatutah001@gmail.com>
2023-03-18 21:52:18 +0700

    Initial commit

---
+++ app/src/main/java/com/ibnu/gitfriend/data/network/services/UserService.kt
@@ -0,0 +1,34 @@
+package com.ibnu.gitfriend.data.network.services
+
+import com.ibnu.gitfriend.data.model.User
+import com.ibnu.gitfriend.data.network.response.UserSearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface UserService {
+
+    @GET("users")
+    suspend fun fetchUsers(): List<User>
+
+    @GET("users/{username}")
+    suspend fun fetchUserDetail(
+        @Path("username") username: String
+    ): User
+
+    @GET("search/users")
+    suspend fun fetchSearchUserResult(
+        @Query("q") username: String
+    ): UserSearchResponse
+
+    @GET("users/{username}/followers")
+    suspend fun fetchUserFollower(
+        @Path("username") username: String
+    ): List<User>
+
+    @GET("users/{username}/following")
+    suspend fun fetchUserFollowing(
+        @Path("username") username: String
+    ): List<User>
+
+}
